795af3a0f0f82788f7e43eb5920b44e0c24a869d
JCGokul <jangamchenchugokul@gmail.com>
2024-10-19 22:31:36 +0530

    test_cases

--- rules/tests.py
+++ rules/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
 
 # Create your tests here.
+from .ast import create_rule, evaluate_rule, combine_rules
+
+class RuleEngineTests(TestCase):
+
+    def test_create_rule(self):
+        rule = "age > 30 AND department = 'Sales'"
+        ast = create_rule(rule)
+        self.assertIsNotNone(ast)
+
+    def test_evaluate_rule(self):
+        rule = "age > 30 AND department = 'Sales'"
+        ast = create_rule(rule)
+        data = {"age": 35, "department": "Sales"}
+        result = evaluate_rule(ast, data)
+        self.assertTrue(result)
+
+    def test_combine_rules(self):
+        rules = [
+            "age > 30 AND department = 'Sales'",
+            "age < 25 AND department = 'Marketing'"
+        ]
+        combined_ast = combine_rules(rules)
+        self.assertIsNotNone(combined_ast)
